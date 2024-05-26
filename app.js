5c9c09d3d30a997ea40316429e18c688bda4e947
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-05-26 21:50:15 +0530

    crud implementation

--- app.js
+++ app.js
@@ -1,27 +1,53 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const Blog = require("./models/blog");
+
+// connects to mongodb
+const dbURI =
+  "mongodb+srv://ayeshanweerasuriya:kkbhanuka1@cluster0.aphbi70.mongodb.net/note-tuts?retryWrites=true&w=majority&appName=Cluster0";
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    console.log("Database connected successfully"), app.listen(3000);
+  })
+  .catch((err) => console.log("Database connection error:", err));
+
 const app = express();
 
 // register vue engine
 app.set("view engine", "ejs");
 
-app.listen(3000);
+// make the public folder public
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
+// routes
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-  ];
-  res.render("index", { title: "Home", blogs: blogs });
+  res.redirect("/blogs");
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result });
+    });
+});
+
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get("/about", (req, res) => {
